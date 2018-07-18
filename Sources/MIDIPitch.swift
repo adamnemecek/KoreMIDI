97f5367a90f0d0986a6e3480e44d8641e360e555
Adam Nemecek <adamnemecek@gmail.com>
2018-07-18 10:56:27 -0700

    refactoring

--- Sources/MIDIPitch.swift
+++ Sources/MIDIPitch.swift
@@ -6,7 +6,12 @@
 //
 //
 
-public struct MIDIPitch : Comparable, Hashable, RawRepresentable, Strideable {
+public enum MIDIPitchClass : Int {
+    case c, cs, d, ds, e, f, fs, g, gs, a, `as`, b
+}
+
+public struct MIDIPitch : Comparable, Hashable, RawRepresentable, Strideable, CustomStringConvertible {
+
     public typealias Interval = Int
 
     public static let min = MIDIPitch(0)
@@ -42,46 +47,19 @@ public struct MIDIPitch : Comparable, Hashable, RawRepresentable, Strideable {
         return lhs.rawValue < rhs.rawValue
     }
 
+    public var octave: Int {
+        return Int(rawValue) / 12
+    }
+
+    public var pc: MIDIPitchClass {
+        return MIDIPitchClass(rawValue: Int(rawValue) % 12)!
+    }
+
     public var hashValue: Int {
         return rawValue.hashValue
     }
-}
 
-//public struct MIDIDrum : Comparable, Hashable, RawRepresentable, Strideable {
-//    public typealias Interval = Int
-//    private static let range = 0..<Int8.max
-//
-//    public let rawValue: Int8
-//
-//    public init?(rawValue: Int8) {
-//        self.rawValue = rawValue
-//    }
-//
-//    public init(_ value: Int8) {
-//        self.rawValue = value
-//    }
-//
-//    public static func +(lhs: MIDIDrum, rhs: Interval) -> MIDIDrum {
-//        fatalError()
-//    }
-//
-//    public func advanced(by n: Interval) -> MIDIDrum {
-//        return self + n
-//    }
-//
-//    public func distance(to other: MIDIDrum) -> Interval {
-//        return rawValue.distance(to: other.rawValue)
-//    }
-//
-//    public static func ==(lhs: MIDIDrum, rhs: MIDIDrum) -> Bool {
-//        return lhs.rawValue == rhs.rawValue
-//    }
-//
-//    public static func <(lhs: MIDIDrum, rhs: MIDIDrum) -> Bool {
-//        return lhs.rawValue < rhs.rawValue
-//    }
-//
-//    public var hashValue: Int {
-//        return rawValue.hashValue
-//    }
-//}
+    public var description: String {
+        return "\(octave)\(pc)(\(rawValue)"
+    }
+}
